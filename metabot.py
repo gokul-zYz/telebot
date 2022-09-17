ffc26de5ac1809553b5d9da927cd48c525b350a7
GOKUL SUBRAMANIAN <96585296+GOKUL69@users.noreply.github.com>
2022-09-17 17:04:22 +0530

    Add files via upload

---
+++ metabot.py
@@ -0,0 +1,27 @@
+from telegram.ext import *
+import response as R
+print("SIGMA WAKES UP...5..")
+api=("5748788062:AAG0wd6J8MMoB_FDZXFelYc12xOkz5i2t5c")
+updater = Updater(token="5748788062:AAG0wd6J8MMoB_FDZXFelYc12xOkz5i2t5c")
+def start_command(update,context: CallbackContext):
+    update.message.reply_text("Im Wake Up! bro")
+def help_command(update,context: CallbackContext):
+    update.message.reply_text("Need Help!? huh whamenn!")
+def handle_message(update,context: CallbackContext):
+    text=str(update.message.text).lower()
+    response=R.sample_responses(text,context,update)
+    update.message.reply_text(response)
+def error(update,context):
+    print(f"Update{update}caused error: {context.error}")
+def main():
+    updater=Updater(api,use_context=True)
+    dp=updater.dispatcher
+    dp.add_handler(CommandHandler("start",start_command))
+    dp.add_handler(CommandHandler("help",help_command))
+    dp.add_handler(MessageHandler(Filters.text,handle_message))
+    dp.add_error_handler(error)
+    updater.start_polling()
+    updater.idle()
+    
+    
+main()
---
+++ response.py
@@ -0,0 +1,45 @@
+from email.mime import text
+from telegram.ext import *
+from requests import*
+from telegram import *
+import schedule
+import telegram,requests
+chat_list=[5645358157, 1339241817]
+#api_key="5748788062:AAHPP-5baIzIy7Q7zjccYgnwIJkAzjqRUeE"
+def sample_responses(input_text,context,update):
+    botl=telegram.Bot("5748788062:AAG0wd6J8MMoB_FDZXFelYc12xOkz5i2t5c")
+    user_message=str(input_text).lower()
+    chat_id=update.effective_chat.id
+    if chat_id  not in chat_list:
+        chat_list.append(chat_id)
+    print(chat_list)
+    if user_message in("hi","hola","vanakkam","namaste","hy","hyy"):
+         image = get("https://pbs.twimg.com/media/EJ8gLIcVAAI-WQw.jpg").content
+         botl.send_chat_action(chat_id,action=telegram.chataction.ChatAction.TYPING)
+         return context.bot.sendMediaGroup(chat_id, media=[InputMediaPhoto(image, caption="வணக்கம்டி மாப்ளே")])
+    if user_message in("fuck","sex","boobs"):
+         image = get("https://pbs.twimg.com/media/FZFchgwakAASIux.jpg").content
+         botl.send_chat_action(chat_id,action=telegram.chataction.ChatAction.TYPING)
+         return context.bot.sendMediaGroup(chat_id, media=[InputMediaPhoto(image, caption="வேண்டம் அண்ணா😞")])
+    if user_message in("Theme song","our song","song","depression","boosts"):
+        try :
+             botl.send_message(chat_id,text="Wait Few Secs Im a Little Bot......😥")
+             image = get("https://od.lk/d/NDBfNTAyMDg1Mzdf/GIGA%20CHAD%201.mp3").content
+             botl.send_chat_action(chat_id,action=telegram.chataction.ChatAction.UPLOAD_AUDIO)
+             return context.bot.sendMediaGroup(chat_id, media=[InputMediaAudio(image, caption="")])
+        except:
+          return("BOT UNDER MAINTAINANCE💻")
+    if user_message in("send"):
+        for i in chat_list:
+            try :
+                botl.send_message(i,text="MoVe Towards Your Goal Bro🐱‍🏍")
+                image = get("https://od.lk/d/NDBfNTAyMDg1Mzdf/GIGA%20CHAD%201.mp3").content
+                botl.send_chat_action(i,action=telegram.chataction.ChatAction.UPLOAD_AUDIO)
+                return context.bot.sendMediaGroup(i, media=[InputMediaAudio(image, caption="")])
+            except:
+              return("BOT UNDER MAINTAINANCE💻")
+    return "nah Bro! cant understand exactly 😪What youre asking For Try Again!🧐"
+    
+
+if __name__ == "__main__":
+    sample_responses =()
